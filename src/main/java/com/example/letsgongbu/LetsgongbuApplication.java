1fa9492fb082e4b7aeaa39d6b966c17a2745448e
Shinnybest <kim295521@naver.com>
2022-06-28 19:47:35 +0900

    [CHORE] 환경설정 후 첫 커밋

---
+++ src/main/java/com/example/letsgongbu/LetsgongbuApplication.java
@@ -0,0 +1,13 @@
+package com.example.letsgongbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsgongbuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsgongbuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/letsgongbu/LetsgongbuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.letsgongbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsgongbuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
