5536b89f69cf77390b15fd4876874f39e81b7de2
Shinnybest <kim295521@naver.com>
2022-07-04 13:44:59 +0900

    [FEAT] 생성, 최종수정일자 기록하는 TimeStamped 추상 클래스 생성 후 Post, Comment에 적용

--- src/main/java/com/example/letsgongbu/LetsgongbuApplication.java
+++ src/main/java/com/example/letsgongbu/LetsgongbuApplication.java
@@ -2,8 +2,10 @@ package com.example.letsgongbu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LetsgongbuApplication {
 
     public static void main(String[] args) {
