86765591a28e25ed6c4c5a67e22aeff4584da380
Shinnybest <kim295521@naver.com>
2022-07-11 19:58:04 +0900

    [FEAT] rememberMe, logout 기능 추가 및 bcrypt로 passwordEncoder 추가 및 설정

--- src/main/java/com/example/letsgongbu/config/SecurityConfiguration.java
+++ src/main/java/com/example/letsgongbu/config/SecurityConfiguration.java
@@ -1,51 +1,55 @@
 package com.example.letsgongbu.config;
 
 import com.example.letsgongbu.repository.MemberRepository;
+import com.example.letsgongbu.security.AuthenticationSuccessHandlerImpl;
 import com.example.letsgongbu.security.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
-
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     private final MemberRepository memberRepository;
     private final DataSource dataSource;
-
+    private final AuthenticationSuccessHandlerImpl successHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .antMatchers("/", "/login", "/logout", "/posts", "/studyroom/all", "/sign-up")
+                        .antMatchers("/", "/login", "/posts", "/studyroom/all", "/sign-up")
                         .permitAll()
-                        .anyRequest().authenticated()
-                )
+                        .anyRequest().authenticated())
                 .formLogin(formLogin -> formLogin
-                        .loginPage("/login"))
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .successHandler(successHandler)
+                        .defaultSuccessUrl("/"))
                 .rememberMe(rememberMe -> rememberMe
-                        .key("rememberMeTest")
-//                        .alwaysRemember(false)
-                        .rememberMeServices(rememberMeServices(persistentTokenRepository()))
-//                        .rememberMeParameter("remember-me")
-//                        .rememberMeCookieName("remember-me")
-//                        .rememberMeCookieDomain("http://localhost:8080")
-//                .useSecureCookie() // https 사용
-//                        .tokenRepository(persistentTokenRepository())
-                        .userDetailsService(new UserDetailsServiceImpl(memberRepository))
-//                .authenticationSuccessHandler()
-//                        .tokenValiditySeconds(1000)
-                        )
+                                .key("rememberMeKey")
+                                .tokenRepository(persistentTokenRepository())
+                                .rememberMeServices(rememberMeServices(persistentTokenRepository()))
+                                .userDetailsService(new UserDetailsServiceImpl(memberRepository)))
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true))
                 .httpBasic().disable();
         return http.build();
     }
@@ -58,11 +62,19 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public RememberMeServices rememberMeServices(PersistentTokenRepository tokenRepository){
+    public PersistentTokenBasedRememberMeServices rememberMeServices(PersistentTokenRepository tokenRepository) {
         PersistentTokenBasedRememberMeServices rememberMeServices = new
-                PersistentTokenBasedRememberMeServices("rememberMeTest", new UserDetailsServiceImpl(memberRepository), tokenRepository);
+                PersistentTokenBasedRememberMeServices("rememberMeKey", new UserDetailsServiceImpl(memberRepository), tokenRepository);
         rememberMeServices.setParameter("remember-me");
-        rememberMeServices.setCookieName("remember-me");
+        rememberMeServices.setAlwaysRemember(true);
         return rememberMeServices;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        String idForEncode = "bcrypt";
+        Map encoders = new HashMap<>();
+        encoders.put(idForEncode, new BCryptPasswordEncoder());
+        return new DelegatingPasswordEncoder(idForEncode, encoders);
     }
 }
