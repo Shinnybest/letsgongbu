2cda133e8bf3e0a7776400cee2858b3237689e40
Shinnybest <kim295521@naver.com>
2022-07-25 23:41:06 +0900

    [FIX] @EnableJpaAuditing 어노테이션 위치 애플리케이션 실행 시점에서 Configuration 클래스 만들어서 실행으로 변경

---
+++ src/main/java/com/example/letsgongbu/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.example.letsgongbu;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
--- src/main/java/com/example/letsgongbu/LetsgongbuApplication.java
+++ src/main/java/com/example/letsgongbu/LetsgongbuApplication.java
@@ -2,10 +2,8 @@ package com.example.letsgongbu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class LetsgongbuApplication {
 
     public static void main(String[] args) {
